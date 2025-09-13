8f5d3fbaf138940e55a3cd5f208162cc6a156f78
olmag99 <olmag99@gmail.com>
2025-09-13 14:35:16 +0200

    Create textCodeCheck.js

---
+++ js/textCodeCheck.js
@@ -0,0 +1,12 @@
+const atTheOldToad = {
+  potions: [ ],
+  getPotions() {
+    return "List of all available potions";
+  },
+  addPotion(potionName) {
+    return "Adding ${potionName}";
+  }
+  
+};
+
+atTheOldToad.addPotion("Invisibility")
